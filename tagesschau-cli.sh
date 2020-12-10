4d8b92a0e70ac7f944a04771a229aaa01403f22a
timnickname <timnickname@users.noreply.github.com>
2020-12-10 12:20:30 +0100

    Update tagesschau-cli.sh
    changed from python to bash / curl+jq.

--- tagesschau-cli.sh
+++ tagesschau-cli.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
-echo -e "\e[44m+++ Schlagzeilen von tagesschau.de +++ \e[0m"
-python3 -c "import requests, sys;
-response = requests.get('http://www.tagesschau.de/api2/homepage'); 
-jsonResponse = response.json();
-for i in range(5):
-	print(jsonResponse['news'][i]['topline'], ': ', jsonResponse['news'][i]['title'])"
-echo -e "\e[44mmehr auf https://tagesschau.de.\e[0m"
+#!/bin/bash
+echo -e "\e[44m"
+echo -e "------------------------------"
+echo -e "Top-Schlagzeilen von tageschau.de:"
+for i in {0..4}
+do
+	topline[$i]=$(curl -s  https://www.tagesschau.de/api2/homepage/ | jq -r --arg i "$i" '.news[$i | tonumber].topline')
+	title[$i]=$(curl -s https://www.tagesschau.de/api2/homepage/ | jq -r --arg i "$i" '.news[$i | tonumber].title')
+done
+for i in {0..4}
+do
+	echo -n ${topline[$i]}
+	echo -n ": "
+	echo  ${title[$i]}
+done
+
+echo -e "\e[44mmehr auf https://tagesschau.de"
+echo -e "------------------------------\e[0m"
+echo ""
